e8f49200b2dc7ba367d98093a22733cad7cc35e0
Chad Eldridge <chad.eldridge@gmail.com>
2024-06-21 18:32:03 -0400

    Made struct public.

--- vectors/vector2.go
+++ vectors/vector2.go
@@ -5,17 +5,17 @@ import (
 	"math"
 )
 
-type vector2 struct {
+type Vector2 struct {
 	X float64 `json:"x"`
 	Y float64 `json:"y"`
 }
 
 // NewVector2 creates a new Vector2 with the given x and y values.
-func NewVector2(x, y float64) vector2 {
-	return vector2{X: x, Y: y}
+func NewVector2(x, y float64) Vector2 {
+	return Vector2{X: x, Y: y}
 }
 
-func (v vector2) Copy() vector2 {
+func (v Vector2) Copy() Vector2 {
 	return NewVector2(v.X, v.Y)
 }
 
@@ -23,43 +23,43 @@ func (v vector2) Copy() vector2 {
 //		Operators		//
 //					//
 
-func (v vector2) Add(s float64) vector2 {
+func (v Vector2) Add(s float64) Vector2 {
 	return NewVector2(v.X+s, v.Y+s)
 }
 
-func (v vector2) AddVector(v2 vector2) vector2 {
+func (v Vector2) AddVector(v2 Vector2) Vector2 {
 	return NewVector2(v.X+v2.X, v.Y+v2.Y)
 }
 
-func (v vector2) Sub(s float64) vector2 {
+func (v Vector2) Sub(s float64) Vector2 {
 	return NewVector2(v.X-s, v.Y-s)
 }
 
-func (v vector2) SubVector(v2 vector2) vector2 {
+func (v Vector2) SubVector(v2 Vector2) Vector2 {
 	return NewVector2(v.X-v2.X, v.Y-v2.Y)
 }
 
-func (v vector2) Mul(s float64) vector2 {
+func (v Vector2) Mul(s float64) Vector2 {
 	return NewVector2(v.X*s, v.Y*s)
 }
 
-func (v vector2) MulVector(v2 vector2) vector2 {
+func (v Vector2) MulVector(v2 Vector2) Vector2 {
 	return NewVector2(v.X*v2.X, v.Y*v2.Y)
 }
 
-func (v vector2) Div(s float64) vector2 {
+func (v Vector2) Div(s float64) Vector2 {
 	return NewVector2(v.X/s, v.Y/s)
 }
 
-func (v vector2) DivVector(v2 vector2) vector2 {
+func (v Vector2) DivVector(v2 Vector2) Vector2 {
 	return NewVector2(v.X/v2.X, v.Y/v2.Y)
 }
 
-func (v vector2) Neg() vector2 {
+func (v Vector2) Neg() Vector2 {
 	return NewVector2(-v.X, -v.Y)
 }
 
-func (v vector2) Equal(v2 vector2) bool {
+func (v Vector2) Equal(v2 Vector2) bool {
 	return v.X == v2.X && v.Y == v2.Y
 }
 
@@ -67,11 +67,11 @@ func (v vector2) Equal(v2 vector2) bool {
 //		Methods			//
 //					//
 
-func (v vector2) Angle(v2 vector2) float64 {
+func (v Vector2) Angle(v2 Vector2) float64 {
 	return math.Acos(v.Normalize().Dot(v2.Normalize()))
 }
 
-func (v vector2) ClampMagnitude(max float64) vector2 {
+func (v Vector2) ClampMagnitude(max float64) Vector2 {
 	if v.Magnitude() > max {
 		return v.Normalize().Mul(max)
 	}
@@ -79,35 +79,35 @@ func (v vector2) ClampMagnitude(max float64) vector2 {
 	return v.Copy()
 }
 
-func (v vector2) Cross(v2 vector2) float64 {
+func (v Vector2) Cross(v2 Vector2) float64 {
 	return v.X*v2.Y - v.Y*v2.X
 }
 
-func (v vector2) Distance(v2 vector2) float64 {
+func (v Vector2) Distance(v2 Vector2) float64 {
 	return v.SubVector(v2).Magnitude()
 }
 
-func (v vector2) Dot(v2 vector2) float64 {
+func (v Vector2) Dot(v2 Vector2) float64 {
 	return v.X*v2.X + v.Y*v2.Y
 }
 
-func (v vector2) Lerp(v2 vector2, t float64) vector2 {
+func (v Vector2) Lerp(v2 Vector2, t float64) Vector2 {
 	return v.AddVector(v2.SubVector(v).Mul(t))
 }
 
-func (v vector2) Magnitude() float64 {
+func (v Vector2) Magnitude() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v vector2) Max(v2 vector2) vector2 {
+func (v Vector2) Max(v2 Vector2) Vector2 {
 	return NewVector2(math.Max(v.X, v2.X), math.Max(v.Y, v2.Y))
 }
 
-func (v vector2) Min(v2 vector2) vector2 {
+func (v Vector2) Min(v2 Vector2) Vector2 {
 	return NewVector2(math.Min(v.X, v2.X), math.Min(v.Y, v2.Y))
 }
 
-func (v vector2) Normalize() vector2 {
+func (v Vector2) Normalize() Vector2 {
 	if m := v.Magnitude(); m > 0 {
 		return v.Div(m)
 	}
@@ -115,10 +115,10 @@ func (v vector2) Normalize() vector2 {
 	return v.Copy()
 }
 
-func (v vector2) Vector3() vector3 {
+func (v Vector2) Vector3() Vector3 {
 	return NewVector3(v.X, v.Y, 0)
 }
 
-func (v vector2) String() string {
+func (v Vector2) String() string {
 	return fmt.Sprintf("(%f, %f)", v.X, v.Y)
 }
--- vectors/vector3.go
+++ vectors/vector3.go
@@ -2,18 +2,18 @@ package vectors
 
 import "fmt"
 
-type vector3 struct {
-	vector2
+type Vector3 struct {
+	Vector2
 	Z float64
 }
 
 // NewVector3 creates a new Vector3 with the given x, y, and z values.
 // Vector3 uses a Vector2 object for x and y so Vector2 fuctions can be used on Vector3.
-func NewVector3(x, y, z float64) vector3 {
-	return vector3{vector2: vector2{X: x, Y: y}, Z: z}
+func NewVector3(x, y, z float64) Vector3 {
+	return Vector3{Vector2: Vector2{X: x, Y: y}, Z: z}
 }
 
-func (v vector3) Copy() vector3 {
+func (v Vector3) Copy() Vector3 {
 	return NewVector3(v.X, v.Y, v.Z)
 }
 
@@ -21,43 +21,43 @@ func (v vector3) Copy() vector3 {
 //		  Math			//
 //					//
 
-func (v vector3) Add(s float64) vector3 {
+func (v Vector3) Add(s float64) Vector3 {
 	return NewVector3(v.X+s, v.Y+s, v.Z+s)
 }
 
-func (v vector3) AddVector(v2 vector3) vector3 {
+func (v Vector3) AddVector(v2 Vector3) Vector3 {
 	return NewVector3(v.X+v2.X, v.Y+v2.Y, v.Z+v2.Z)
 }
 
-func (v vector3) Sub(s float64) vector3 {
+func (v Vector3) Sub(s float64) Vector3 {
 	return NewVector3(v.X-s, v.Y-s, v.Z-s)
 }
 
-func (v vector3) SubVector(v2 vector3) vector3 {
+func (v Vector3) SubVector(v2 Vector3) Vector3 {
 	return NewVector3(v.X-v2.X, v.Y-v2.Y, v.Z-v2.Z)
 }
 
-func (v vector3) Mul(s float64) vector3 {
+func (v Vector3) Mul(s float64) Vector3 {
 	return NewVector3(v.X*s, v.Y*s, v.Z*s)
 }
 
-func (v vector3) MulVector(v2 vector3) vector3 {
+func (v Vector3) MulVector(v2 Vector3) Vector3 {
 	return NewVector3(v.X*v2.X, v.Y*v2.Y, v.Z*v2.Z)
 }
 
-func (v vector3) Div(s float64) vector3 {
+func (v Vector3) Div(s float64) Vector3 {
 	return NewVector3(v.X/s, v.Y/s, v.Z/s)
 }
 
-func (v vector3) DivVector(v2 vector3) vector3 {
+func (v Vector3) DivVector(v2 Vector3) Vector3 {
 	return NewVector3(v.X/v2.X, v.Y/v2.Y, v.Z/v2.Z)
 }
 
-func (v vector3) Neg() vector3 {
+func (v Vector3) Neg() Vector3 {
 	return NewVector3(-v.X, -v.Y, -v.Z)
 }
 
-func (v vector3) Equal(v2 vector3) bool {
+func (v Vector3) Equal(v2 Vector3) bool {
 	return v.X == v2.X && v.Y == v2.Y && v.Z == v2.Z
 }
 
@@ -65,24 +65,24 @@ func (v vector3) Equal(v2 vector3) bool {
 //		Methods			//
 //					//
 
-func (v vector3) Distance(v2 vector3) float64 {
+func (v Vector3) Distance(v2 Vector3) float64 {
 	return 0
 	// return v.Sub(v2).Magnitude()
 }
 
-func (v vector3) Vector2() vector2 {
-	return v.vector2
+func (v Vector3) ToVector2() Vector2 {
+	return v.Vector2.Copy()
 }
 
-func (v vector3) String() string {
+func (v Vector3) String() string {
 	return fmt.Sprintf("(%f, %f, %f)", v.X, v.Y, v.Z)
 }
 
-func (v vector3) MarshalJSON() ([]byte, error) {
+func (v Vector3) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`{"x":%f,"y":%f,"z":%f}`, v.X, v.Y, v.Z)), nil
 }
 
-func (v *vector3) UnmarshalJSON(data []byte) error {
+func (v *Vector3) UnmarshalJSON(data []byte) error {
 	var x, y, z float64
 	if _, err := fmt.Sscanf(string(data), `{"x":%f,"y":%f,"z":%f}`, &x, &y, &z); err != nil {
 		return err
